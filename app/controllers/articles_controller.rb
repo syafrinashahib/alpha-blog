f8a5f00f882c0b9ecbd25e1d9a0cd67d801f5b6b
Syafrina <syaf@yhaoo.com>
2016-07-15 16:48:15 +0000

    Add edit, update, index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
     
+    def index
+        @articles = Article.all
+    end
+    
     def new
         @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
@@ -10,14 +18,23 @@ class ArticlesController < ApplicationController
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
         else
-            
             render 'new'
         end
         
-        def show
-            @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
         end
-        
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
     private
